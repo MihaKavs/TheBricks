4d4eba485e3d022768af4db89c0a62e7021c8d6b
MihaKavs <95538899+MihaKavs@users.noreply.github.com>
2022-04-13 15:12:13 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,162 +1,222 @@
 function creddit() {
   Swal.fire({
-    title: 'Project made Miha Kavs',
+    title: 'Made Miha Kavs',
     confirmButtonText: 'Okej',
+    text: 'This game was made for a school project.',
     color: '#000000',
     confirmButtonColor: '#000000',
   })
 }
-
-function drawIt() {
-  var x = 300;
-  var y = 300;
-  var dx = 2;
-  var dy = 4;
-  var WIDTH;
-  var HEIGHT;
-  var r = 10;
-  var ctx;
-  function win(){
-    clearInterval(intTimer);
-    clearInterval(interval);
-    var ctx;
-    ctx = $('#canvas')[0].getContext("2d");
-    WIDTH = $("#canvas").width();
-    HEIGHT = $("#canvas").height();
-    ctx.clearRect(0, 0, WIDTH, HEIGHT);
-    circle(x, y, 10);
-    Swal.fire({
-      title: 'Congratulations you won!!',
-      confirmButtonText: 'Restart',
-      color: '#000000',
-      confirmButtonColor: '#000000',
-    }).then((result) => {
-      countdown = setInterval(countdown,10);
-  
-    })
-  }
-  function init() {
-    ctx = $('#canvas')[0].getContext("2d");
-    document.getElementById("center").style.width = "60%";
-    WIDTH = $("#canvas").width();
-    HEIGHT = $("#canvas").height();
-
-    tocke = 0;
-    $("#tocke").html("Points: " + "<br/>" +  tocke);
+function restart() {
+  document.getElementById("restart").style.display = "none";
+  countDown = setInterval(countdown, 10);
+}
+function countdown() {
+  if (tocke == 0) {
     sekunde = 0;
-    izpisTimer = "00:00";
-    intTimer = setInterval(timer, 1000);
-    return interval = setInterval(draw, 10);
+    $("#tocke").html("Points: " + "<br/>" + tocke);
+    clearInterval(countDown);
+    initbricks();
+    x = 400;
+    y = 400;
+    dx = 2;
+    dy = 4;
+    draw();
+    intTimer = setInterval(timer, 2000);
+    setTimeout(() => { interval = setInterval(draw, 10) }, 1000);
+    tocke++;
   }
-  function circle(x, y, r) {
-    ctx.fillStyle = "#a60100";
-    ctx.beginPath();
-    ctx.arc(x, y, r, 0, Math.PI * 2, true);
-    ctx.closePath();
-    ctx.fill();
-  }
-  function rect(x, y, w, h) {
-    ctx.beginPath();
-    ctx.rect(x, y, w, h);
-    ctx.closePath();
-    ctx.fill();
-  }
-  function clear() {
-    ctx.clearRect(0, 0, WIDTH, HEIGHT);
-  }
-  function draw() {
-    clear();
-    circle(x, y, 10);
-    //premik ploščice levo in desno
-    if (rightDown) {
-      if ((paddlex + paddlew) < WIDTH) {
-        paddlex += 5;
-      } else {
-        paddlex = WIDTH - paddlew;
-      }
+  tocke--;
+  $("#tocke").html("Points: " + "<br/>" + tocke);
+  sliderHeight();
+}
+function lose() {
+  Swal.fire({
+    title: 'Uh oh you lost!!' + '<br/>' + 'You got: ' + tocke + ' points' + '<br/>' + 'Time: ' + izpisTimer,
+    confirmButtonText: 'Restart',
+    showDenyButton: true,
+    denyButtonText: 'Ok',
+    color: '#000000',
+    confirmButtonColor: '#000000',
+  }).then((result) => {
+    if (result.isConfirmed) {
+      x = 400;
+      y = 400;
+      dx = 2;
+      dy = 4;
+      ctx.globalAlpha = 0.1;
+      countDown = setInterval(countdown, 10);
+    } else if (result.isDenied) {
+      document.getElementById("restart").style.display = "block";
     }
-    else if (leftDown) {
-      if (paddlex > 0) {
-        paddlex -= 5;
-      } else {
-        paddlex = 0;
-      }
-    }
-    paddleDrunknes();
-    ctx.drawImage(plate,paddlex, HEIGHT - paddleh, paddlew, paddleh);
-    for (i = 0; i < NROWS; i++) {
-      for (j = 0; j < NCOLS; j++) {
-        if (i == 0) {
-          if(bricks[i][j] != 0){
-            /*rect((j * (BRICKWIDTH + PADDING)) + PADDING,
-            (i * (BRICKHEIGHT + PADDING)) + PADDING,
-            BRICKWIDTH, BRICKHEIGHT);*/
-            if(j ==  Math.floor(NCOLS/2))
-              ctx.drawImage(keg,j*BRICKWIDTH,i * BRICKHEIGHT ,BRICKWIDTH,BRICKHEIGHT);
-            else
-             ctx.drawImage(beer,j*BRICKWIDTH,i * BRICKHEIGHT ,BRICKWIDTH,BRICKHEIGHT);
-          }
-            
-        }
-		    if (i == 1) {
-          if(bricks[i][j] != 0)
-            ctx.drawImage(coctail,j*BRICKWIDTH,i * BRICKHEIGHT ,BRICKWIDTH,BRICKHEIGHT);
-          /*rect((j * (BRICKWIDTH + PADDING)) + PADDING,
-            (i * (BRICKHEIGHT + PADDING)) + PADDING,
-          BRICKWIDTH, BRICKHEIGHT);*/
-        }
-        if(i == 2){
-		      if (bricks[i][j] != 0) 
-           ctx.drawImage(wine,j*BRICKWIDTH,i * BRICKHEIGHT ,BRICKWIDTH,BRICKHEIGHT);
-          /*rect((j * (BRICKWIDTH + PADDING)) + PADDING,
-            (i * (BRICKHEIGHT + PADDING)) + PADDING,
-            BRICKWIDTH, BRICKHEIGHT);*/
-        }
-      }
-    }
-    rowheight = BRICKHEIGHT  + r / 2; //Smo zadeli opeko?
-    colwidth = BRICKWIDTH  + r / 2;
-    row = Math.floor(y / rowheight);
-    col = Math.floor(x / colwidth);
-    //console.log(col);
-    //console.log(Math.floor(NCOLS/2));
-    //Če smo zadeli opeko, vrni povratno kroglo in označi v tabeli, da opeke ni več
-    if (y < NROWS * rowheight && row >= 0 && col >= 0 && bricks[row][col] != 0) {
-      dy = -dy; bricks[row][col] -= 1;
-      if(bricks[row][col] == 0){
-        if(row == 2)
-          tocke += 5;
-        else if(row == 1)
-          tocke += 10;
-        else if(row == 0 && col == Math.floor(NCOLS/2))
-          tocke += 50;
-        else
-          tocke += 15;
-        sliderHeight();
-        if(document.getElementById("slider").style.backgroundPositionY == "0%")
-          win();
-      }
-   //v primeru, da imajo opeko večjo utež lahko prištevate tudi npr. 2 ali 3; pred tem bi bilo smiselno dodati še kakšen pogoj, ki bi signaliziral mesta opek, ki imajo višjo vrednost
-      $("#tocke").html("Points: " + "<br/>" +  tocke);
-    }
-    if (x + dx > WIDTH - r || x + dx < 0 + r)
-      dx = -dx;
-    if (y + dy < 0 + r)
-      dy = -dy;
-    else if (y + dy > HEIGHT -paddleh/2 - r) {
-      if (x > paddlex && x < paddlex + paddlew) {
-        dx = 8 * ((x - (paddlex + paddlew / 2)) / paddlew);
-        dy = -dy;
-      }
-      else if (y + dy > HEIGHT - r) {
-        clearInterval(intTimer);
-        clearInterval(interval);
-      }
+  })
+}
+function win() {
+  clearInterval(intTimer);
+  clearInterval(interval);
+  var ctx;
+  ctx = $('#canvas')[0].getContext("2d");
+  WIDTH = $("#canvas").width();
+  HEIGHT = $("#canvas").height();
+  ctx.clearRect(0, 0, WIDTH, HEIGHT);
+  circle(x, y, 10);
+  Swal.fire({
+    title: 'Congratulations you won!!' + '<br/>' + 'You got: ' + tocke + ' points' + '<br/>' + 'Time: ' + izpisTimer,
+    confirmButtonText: 'Restart',
+    color: '#000000',
+    confirmButtonColor: '#000000',
+  }).then((result) => {
+    countDown = setInterval(countdown, 10);
+  })
+}
+function init() {
+  ctx = $('#canvas')[0].getContext("2d");
+  document.getElementById("center").style.width = "60%";
+  WIDTH = $("#canvas").width();
+  HEIGHT = $("#canvas").height();
 
+  tocke = 0;
+  $("#tocke").html("Points: " + "<br/>" + tocke);
+  sekunde = 0;
+  izpisTimer = "00:00";
+  intTimer = setInterval(timer, 1000);
+  return interval = setInterval(draw, 10);
+}
+function circle(x, y, r) {
+  ctx.globalAlpha = 1;
+  ctx.fillStyle = "#a60100";
+  ctx.beginPath();
+  ctx.arc(x, y, r, 0, Math.PI * 2, true);
+  ctx.closePath();
+  ctx.fill();
+}
+function clear() {
+  ctx.clearRect(0, 0, WIDTH, HEIGHT);
+}
+//Move the plate with your mouse or arrow buttons and break those bricks. Difrent types of bricks give different amount of points.
+var countDown;
+function draw() {
+  clear();
+  circle(x, y, 10);
+  if (rightDown) {
+    if ((paddlex + paddlew) < WIDTH) {
+      paddlex += 5;
+    } else {
+      paddlex = WIDTH - paddlew;
     }
-    x += dx;
-    y += dy;
   }
+  else if (leftDown) {
+    if (paddlex > 0) {
+      paddlex -= 5;
+    } else {
+      paddlex = 0;
+    }
+  }
+  ctx.globalAlpha = 1;
+  paddleDrunknes();
+  ctx.drawImage(plate, paddlex, HEIGHT - paddleh, paddlew, paddleh);
+  for (i = 0; i < NROWS; i++) {
+    for (j = 0; j < NCOLS; j++) {
+      if (i == 0) {
+        if (bricks[i][j] > 0) {
+          if (j == Math.floor(NCOLS / 2)) {
+            if (bricks[i][j] == 5)
+              ctx.globalAlpha = 1;
+            else if (bricks[i][j] == 4)
+              ctx.globalAlpha = 0.8;
+            else if (bricks[i][j] == 3)
+              ctx.globalAlpha = 0.6;
+            else if (bricks[i][j] == 2)
+              ctx.globalAlpha = 0.4;
+            else
+              ctx.globalAlpha = 0.2;
+            ctx.drawImage(keg, j * BRICKWIDTH, i * BRICKHEIGHT, BRICKWIDTH, BRICKHEIGHT);
+          } else {
+            if (bricks[i][j] == 3)
+              ctx.globalAlpha = 1;
+            else if (bricks[i][j] == 2)
+              ctx.globalAlpha = 0.66;
+            else
+              ctx.globalAlpha = 0.33;
+            ctx.drawImage(beer, j * BRICKWIDTH, i * BRICKHEIGHT, BRICKWIDTH, BRICKHEIGHT);
+          }
+        }
+      }
+      if (i == 1) {
+        if (bricks[i][j] > 0) {
+          if (bricks[i][j] == 2)
+            ctx.globalAlpha = 1;
+          else if (bricks[i][j] == 1)
+            ctx.globalAlpha = 0.5;
+          ctx.drawImage(coctail, j * BRICKWIDTH, i * BRICKHEIGHT, BRICKWIDTH, BRICKHEIGHT);
+
+        }
+      }
+      if (i == 2) {
+        if (bricks[i][j] > 0){
+          ctx.globalAlpha = 1;
+          ctx.drawImage(wine, j * BRICKWIDTH, i * BRICKHEIGHT, BRICKWIDTH, BRICKHEIGHT);
+        }
+      }
+    }
+  }
+  for (i = 0; i < NROWS; i++) {
+    for (j = 0; j < NCOLS; j++) {
+      if (y + (dy * 2) + r <= (BRICKHEIGHT * i) + BRICKHEIGHT && y >= BRICKHEIGHT * i && x >= BRICKWIDTH * j && x <= (BRICKWIDTH * j) + BRICKWIDTH && bricks[i][j] != 0) {
+        dy = dy * (-1); bricks[i][j] -= 1;
+        if (bricks[i][j] == 0) {
+          if (i == 2)
+            tocke += 5;
+          else if (i == 1)
+            tocke += 10;
+          else if (i == 0 && j == Math.floor(NCOLS / 2))
+            tocke += 50;
+          else
+            tocke += 15;
+          sliderHeight();
+          if (document.getElementById("slider").style.backgroundPositionY == "0%")
+            win();
+        }
+        $("#tocke").html("Points: " + "<br/>" + tocke);
+      } else if (x + dx + r >= BRICKWIDTH * j && x + dx + r <= (BRICKWIDTH * j) + BRICKWIDTH && y <= (BRICKHEIGHT * i) + BRICKHEIGHT - r && y >= BRICKHEIGHT * i - r && bricks[i][j] != 0) {
+        dx = -dx; bricks[i][j] -= 1;
+        if (bricks[i][j] == 0) {
+          if (i == 2)
+            tocke += 5;
+          else if (i == 1)
+            tocke += 10;
+          else if (i == 0 && j == Math.floor(NCOLS / 2))
+            tocke += 50;
+          else
+            tocke += 15;
+          sliderHeight();
+          if (document.getElementById("slider").style.backgroundPositionY == "0%")
+            win();
+        }
+        $("#tocke").html("Points: " + "<br/>" + tocke);
+      }
+    }
+  }
+  if (x + dx > WIDTH - r || x + dx < 0 + r)
+    dx = -dx;
+  if (y + dy < 0 + r)
+    dy = -dy;
+  else if (y + dy > HEIGHT - paddleh / 2 - r) {
+    if (x > paddlex && x < paddlex + paddlew) {
+      dx = 8 * ((x - (paddlex + paddlew / 2)) / paddlew);
+      dy = -dy;
+    }
+    else if (y + dy > HEIGHT - r) {
+      lose();
+      clearInterval(intTimer);
+      clearInterval(interval);
+    }
+  }
+  x += dx;
+  y += dy;
+}
+function drawIt() {
+  document.getElementById("restart").style.display = "none";
   init();
   init_paddle();
   init_mouse();
@@ -171,23 +231,19 @@ function drawIt() {
     canvasMinX = $("#canvas").offset().left;
     canvasMaxX = canvasMinX + WIDTH;
   }
-
   function onMouseMove(evt) {
     if (evt.pageX > canvasMinX && evt.pageX < canvasMaxX) {
       paddlex = evt.pageX - canvasMinX - paddlew / 2;
     }
   }
   $(document).mousemove(onMouseMove);
-
-
 }
 function timer() {
   sekunde++;
   sekundeI = ((sekundeI = (sekunde % 60)) > 9) ? sekundeI : "0" + sekundeI;
   minuteI = ((minuteI = Math.floor(sekunde / 60)) > 9) ? minuteI : "0" + minuteI;
   izpisTimer = minuteI + ":" + sekundeI;
-
-  $("#cas").html(izpisTimer);
+  $("#cas").html("Time:<br/>" + izpisTimer);
 }
 var keg = new Image();
 keg.src = "img/keg.png";
@@ -204,7 +260,7 @@ var paddleh;
 var paddlew;
 var canvasMinX;
 var canvasMaxX;
-var tocke;
+var tocke = 0;
 var sekunde;
 var sekundeI;
 var minuteI;
@@ -217,7 +273,14 @@ var procentageOfPoints;
 var drunkHeight;
 var fixedPoints;
 var fulDrunkHeight;
-
+var x = 400;
+var y = 400;
+var dx = 2;
+var dy = 4;
+var WIDTH;
+var HEIGHT;
+var r = 10;
+var ctx;
 var bricks;
 var NROWS;
 var NCOLS;
@@ -228,52 +291,41 @@ function initbricks() {
   WIDTH = $("#canvas").width();
   NROWS = 3;
   NCOLS = 8;
-  BRICKWIDTH = (WIDTH / NCOLS) -1 ;
-  BRICKHEIGHT = 80;
+  BRICKWIDTH = (WIDTH / NCOLS) - 1;
+  BRICKHEIGHT = 100;
   PADDING = 1;
   bricks = new Array(NROWS);
-  console.log(Math.floor(NCOLS/2));
+  console.log(Math.floor(NCOLS / 2));
   for (i = 0; i < NROWS; i++) {
     bricks[i] = new Array(NCOLS);
     for (j = 0; j < NCOLS; j++) {
-	  if(i == 0){
-      if(j ==  Math.floor(NCOLS/2)){
-		    bricks[i][j] = 5;
-        console.log("ja");
+      if (i == 0) {
+        if (j == Math.floor(NCOLS / 2)) {
+          bricks[i][j] = 5;
+          console.log("ja");
+        }
+        else
+          bricks[i][j] = 3;
       }
+      else if (i == 1)
+        bricks[i][j] = 2;
       else
-        bricks[i][j] = 3;
-    }
-	  else if(i == 1)
-		bricks[i][j] = 2;
-	else 
-		bricks[i][j] = 1;
+        bricks[i][j] = 1;
     }
   }
 }
-function calculatePionts(){
-  maxPoints = 50+(NCOLS-1)*15+NCOLS*10+NCOLS*5;
+function calculatePionts() {
+  maxPoints = 50 + (NCOLS - 1) * 15 + NCOLS * 10 + NCOLS * 5;
 }
-function sliderHeight(){
-  remainingPoints = maxPoints-tocke;
-  procentageOfPoints = remainingPoints/maxPoints;
+function sliderHeight() {
+  remainingPoints = maxPoints - tocke;
+  procentageOfPoints = remainingPoints / maxPoints;
   fixedPoints = procentageOfPoints.toFixed(2);
-  fixedPoints = 1-fixedPoints;
-  drunkHeight = fixedPoints*200;
+  fixedPoints = 1 - fixedPoints;
+  drunkHeight = fixedPoints * 200;
   fulDrunkHeight = drunkHeight.toFixed();
-  fulDrunkHeight = 200-fulDrunkHeight;
-  document.getElementById("slider").style.backgroundPositionY = -fulDrunkHeight+ "%";
-}
-
-function countdown(){
-  tocke--;
-  sliderHeight();
-  if(tocke == 0){
-    sekunde = 0;
-    intTimer = setInterval(timer, 1000);
-    return interval = setInterval(draw, 10);
-  }
-
+  fulDrunkHeight = 200 - fulDrunkHeight;
+  document.getElementById("slider").style.backgroundPositionY = -fulDrunkHeight + "%";
 }
 var texts = document.getElementsByClassName("fontStyling");
 var randomX;
